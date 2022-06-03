51620cd2cf951d889a1feca9b68a2a58d12afaeb
Patrick Lorio <dev@plorio.com>
2022-06-03 12:32:40 -0700

    Use relative paths and add docker-attach

---
+++ docker-attach.sh
@@ -0,0 +1,1 @@
+docker exec -it lichess /bin/bash
--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+DIR="$( dirname -- "$BASH_SOURCE"; )";
+LICHESS_PATH=$(readlink -f "${DIR}/../lichess-server")
+
 docker run \
-    --mount type=bind,source=$HOME/dev/lichess,target=/home/lichess/projects \
+    --mount type=bind,source=$LICHESS_PATH,target=/home/lichess/projects \
     --publish 9663:9663 \
     --publish 9664:9664 \
     --publish 8212:8212 \
     --name lichess \
     --interactive \
+    --rm \
     --tty \
     lichess
