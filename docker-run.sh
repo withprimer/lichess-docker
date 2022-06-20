4bc4a3bbc9d12c3ae9efdfc812bced6b9986ecb5
katmh <hello@katmh.com>
2022-06-20 13:50:39 -0700

    Mount all of lichess-dev in Docker container

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 DIR="$( dirname -- "$BASH_SOURCE"; )";
-LICHESS_PATH=$(readlink -f "${DIR}/../lichess-server")
+LICHESS_PATH=$(readlink -f "${DIR}/..")
 
 docker run \
-    --mount type=bind,source=$LICHESS_PATH,target=/home/lichess/projects \
+    --mount type=bind,source=$LICHESS_PATH,target=/home/lichess/lichess-dev \
     --publish 9663:9663 \
     --publish 9664:9664 \
     --publish 8212:8212 \
