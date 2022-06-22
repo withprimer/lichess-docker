14a8e6e52a4258d2a205a0c5954def64ee3f94f8
Bojan Joveski <bjoveski@gmail.com>
2022-06-22 16:08:08 -0700

    add docker-compose with mongo & redis

--- docker-run.sh
+++ docker-run.sh
@@ -7,6 +7,7 @@ docker run \
     --mount type=bind,source=$LICHESS_PATH,target=/home/lichess/lichess-dev \
     --publish 9663:9663 \
     --publish 9664:9664 \
+    --publish 6379:6379 \
     --publish 8212:8212 \
     --name lichess \
     --interactive \
